f80c64b929e6281bf8352e671e0d7703a8b2edfd
Elpastore <abdoulayesadio1997@gmail.com>
2023-02-01 20:00:10 +0000

    Première envoie sur internet

---
+++ program.py
@@ -0,0 +1,7 @@
+print('Veuliez SVP choisir deux chiffres')
+x = int(input('Rentrez le premier chiffres '))
+y = int(input('Rentrez le deuxième chiffres'))
+if x > y :
+    print(x,y)
+else:
+    print(y,x)
